5f52fc8f0e822c2d781deefebd1aaf6ede6cf2fb
Justin Satterfield <jsatterf@andrew.cmu.edu>
2014-05-07 12:55:38 -0400

    working controller (not synced with host code)

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -12,22 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Modified by: 	Justin Satterfield jsatterf@andrew.cmu.edu
-# Date:	  	February 25, 2014
+#               Christopher  Thaw  cthaw@andrew.cmu.edu
+# Date:	  	April  25, 2014
 #
-# References:	Flow_Stat https://github.com/hip2b2/poxstuff/blob/master/flow_stats.py
+# References:
 #		OpenFlow Tutorial http://archive.openflow.org/wk/index.php/OpenFlow_Tutorial
 #		POX Wiki https://openflow.stanford.edu/display/ONL/POX+Wiki
+#       PyCrypto Toolkit  https://www.dlitz.net/software/pycrypto/
 
 
-
-"""
-This component is for use with the OpenFlow tutorial.
-
-It acts as a simple hub, but can be modified to act like an L2
-learning switch.
-
-It's roughly similar to the one Brandon Heller did for NOX.
-"""
 
 
 from pox.core import core
@@ -76,11 +69,11 @@ class Controller (object):
     self.mac_to_port = {}
     self.ip_to_port = {}
     self.priority=20
-
+    self.verfied=False
     self.key_directory = keydir
 
 
-    #self.digest = self.importDigest("Message")
+    self.digest = self.importDigest("Message")
 
     self.private_key = None
     self.public_key = None
@@ -91,46 +84,6 @@ class Controller (object):
 
     self.load_controller_keys()
 
-
-    #code for simple NAT behavior not performing as expected
-    """
-    if dpid_to_str(connection.dpid)=="7":
-	msg1 = of.ofp_flow_mod()
-    	msg2 = of.ofp_flow_mod()
-    	msg1.priority = msg2.priority = 200
-    	msg1.match.nw_src=IPAddr("10.0.0.1")
-    	msg2.match.nw_src=IPAddr("10.0.0.2")
-
-    	msg1.actions.append(of.ofp_action_nw_addr.set("10.0.1.0"))
-    	msg2.actions.append(of.ofp_action_nw_addr.set("10.0.1.0"))
-	msg1.actions.append(of.ofp_action_output(port = out_port))
-    	msg2.actions.append(of.ofp_action_output(port = out_port))
-    	self.connection.send(msg1)
-    	self.connection.send(msg2)
-
-  	msg3 = of.ofp_flow_mod()
-    	msg4 = of.ofp_flow_mod()
-    	msg3.priority = msg4.priority = 2000
-    	msg3.match.nw_dst=IPAddr("10.0.0.1")
-    	msg4.match.nw_dst=IPAddr("10.0.0.2")
-
-
-    	self.connection.send(msg3)
-    	self.connection.send(msg4)
-
-
-    if dpid_to_str(connection.dpid)=="8":
-    	#Setting up rules for blocking ip traffic from h1 or h2  to h3
-    	msg1 = of.ofp_flow_mod()
-    	msg2 = of.ofp_flow_mod()
-    	msg1.priority = msg2.priority = 2000
-    	msg1.match.nw_src=IPAddr("10.0.0.1")
-    	msg2.match.nw_src=IPAddr("10.0.0.2")
-
-
-    	self.connection.send(msg13)
-    	self.connection.send(msg31)
-    """
     default_rule = of.ofp_flow_mod()
     default_rule.priority = 1
     default_rule.actions.append(of.ofp_action_output(port = of.OFPP_CONTROLLER))
@@ -155,22 +108,18 @@ class Controller (object):
   def act_like_switch (self, packet, packet_in):
     #print "*rawdata string:*"+str(packet.raw[53:])+"*:end of string"
     data=packet.raw[54:]
-    if data.__contains__('%'):
-    	print ("Data: {0}".format(data))
-    
+
     ip=packet.find('ipv4')
     tcp=packet.find('tcp')
     index=0
 
     flood=True
-
     AuthPacket=False
     if ip is not None:
 
 
         src_id=""
         dst_id=""
-	"""
         for key in self.hosts:
 
               if str(ip.srcip)==key:
@@ -178,13 +127,7 @@ class Controller (object):
 
               if str(ip.dstip)==key:
     			dst_id=self.hosts[key]
-	"""
-	if self.hosts.__contains__(str(ip.srcip)):
-		src_id=self.hosts[str(ip.srcip)]
-			
-	if self.hosts.__contains__(str(ip.dstip)):
-		dst_id=self.hosts[str(ip.dstip)]
-			
+
         if src_id!="" and dst_id!="":
     		  index=-1
 
@@ -260,7 +203,7 @@ class Controller (object):
                  drop_rule.match.nw_src=ip.srcip
                  drop_rule.match.dl_type=0x0800
                  drop_rule.match.nw_proto=6
-                 drop_rule.actions.append(of.ofp_action_output(port = of.OFPP_NONE))
+                 drop_rule.actions=[]#.append(of.ofp_action_output(port = of.OFPP_NONE))
                  self.connection.send(drop_rule)
                  flood=False
 
@@ -283,6 +226,7 @@ class Controller (object):
                                 rule.match.tp_dst=tcp.dstport
                                 rule.match.nw_dst=ip.dstip
                                 rule.match.nw_src=ip.srcip
+                                rule.match.dl_type=0x0800
                                 rule.match.dl_src=packet.src
                                 rule.match.in_port=packet_in.in_port #dl_dst=packet.dst
                                 print "added "+sw#sw+" packet source "+str(packet.src)+" modifying to value "+str(hash_array[i-1])
@@ -291,6 +235,7 @@ class Controller (object):
                                 rule.match.tp_dst=tcp.dstport
                                 rule.match.nw_dst=ip.dstip
                                 rule.match.nw_src=ip.srcip
+                                rule.match.dl_type=0x0800
                                 rule.match.dl_src=hash_array[len(self.secure_path[index])-i]
 
                                 print "added "+sw #+" packet source "+str(packet.src)+"=? "+str(hash_array[i])+" modifying to value "+str(hash_array[i-1])
@@ -313,61 +258,10 @@ class Controller (object):
 
 
 
-
-    """
-    3. If this exists can use nonce and value included to encrypt with private key of controller and
-   	and generate the flow rules
-    4.  do hash of value n times
-    5. To first switch (switch that sent packet ) send rule that modifies the received packet with the
-	metadata of nth hash
-	add drop rule to all switchets for flow with less priority
-    6.  in each successive switch modify the meta data with n-1, n-2 have second rule that drops flow for
-	anything else
-    7. store in switch keep track of current priority or some value to remove flow if update of secure path
-       happens
-
-    """
     if flood==True:
       self.resend_packet(packet_in, of.OFPP_ALL)
-    """
-    print dpid_to_str(self.connection.dpid)+" Src: "+str(packet.src)+" Dest: "+str(packet.dst)
 
-    self.mac_to_port[str(packet.src)]=packet_in.in_port
-    dest_port=-1
-
-    #if the port associated with the destination MAC of the packet is known:
-    # Send packet out the associated port
-    for key in self.mac_to_port:
-        if key == str(packet.dst):
-           dest_port=self.mac_to_port[key]
-           break
-
-    if dest_port != -1 and  AuthPacket==False:
-
-        #Since flow has not yet been added (placed here since this is the first
-	    #time the flow is seen) packet must be sent out
-        self.resend_packet(packet_in,dest_port)
-
-        fm = of.ofp_flow_mod()
-        fm.priority =10
-        fm.match=of.ofp_match( dl_src=packet.src, dl_dst=packet.dst)
-        fm.actions.append(of.ofp_action_output(port = dest_port))
-        self.connection.send(fm)
-
-        # Maybe the log statement should have source/destination/port?
-        print "Installing flow for dst: "+str(packet.dst)+" on port: "+str(dest_port)
-
-    #else:
-      # Flood the packet out everything but the input port
-      # This part looks familiar, right?
-
-    else:
-
- 	  self.resend_packet(packet_in, of.OFPP_ALL)
-      #print "Flooding Packet"
-    """
-
-
+    self.verified=False
 
 
   def _handle_PacketIn (self, event):
@@ -381,33 +275,37 @@ class Controller (object):
       #return
 
     if packet.type == pkt.ethernet.IP_TYPE:
-	ipv4_packet = event.parsed.find("ipv4")
-	ciphertext = ipv4_packet.raw.split('%', 1)
-	parsed_data = None
-	if len(ciphertext) == 2:
-		ciphertext = eval(ciphertext[1])
-		plaintext = self.private_key.decrypt(ciphertext)
-		parsed_data = self.parse_pkt_data(plaintext)
-		self.digest = self.importDigest(parsed_data[0])
-	public_key = self.find_host_key(parsed_data)
-	print(public_key)
-	if not type(public_key) == type(None) and self.verify_signature(public_key, parsed_data[1]):
-		print ("Machine {0} verified!".format(parsed_data[0]))
-		packet_in = event.ofp #The actual ofp_packet_in message
-		self.act_like_switch(packet, packet_in)
-		
-    else:
-	packet_in = event.ofp # The actual ofp_packet_in message.
-	self.act_like_switch(packet, packet_in)
-        #print("NOT IP TYPE")
+        ipv4_packet = event.parsed.find("ipv4")
+        ciphertext = ipv4_packet.raw.split('%', 1)
+        parsed_data = None
+        public_key=None
 
-    #packet_in = event.ofp # The actual ofp_packet_in message.
-    #self.act_like_switch(packet, packet_in)
+        if len(ciphertext) == 2:
+            try:
+                ciphertext = eval(ciphertext[1])
+                plaintext = self.private_key.decrypt(ciphertext)
+                parsed_data = self.parse_pkt_data(plaintext)
+                self.digest = self.importDigest(parsed_data[0])
+                public_key = self.find_host_key(parsed_data)
+                print(public_key)
+
+            except:
+                print "Error evaluating ciphertext"
+
+        if not type(public_key) == type(None) and self.verify_signature(public_key, parsed_data[1]):
+            print ("Machine {0} verified!".format(parsed_data[0]))
+            self.verified =True
+
+
+    packet_in = event.ofp
+    self.act_like_switch(packet, packet_in)
+
 
   def parse_pkt_data(self, raw_data):
 
 	delim = str("%")
 	parsed_data = raw_data.split(delim, 2)
+	garbage_data = parsed_data.pop(0)
 	return parsed_data
 
   def find_host_key(self, parsed_data=[]):
@@ -422,7 +320,7 @@ class Controller (object):
   def importDigest(self,message):
 
 	digest = SHA256.new()
-	data = b64encode(str(message))
+	data = b64encode(message)
 	digest.update(b64decode(data))
 	return digest
 
@@ -442,13 +340,17 @@ class Controller (object):
 	self.private_key = RSA.importKey(private_keydata)
 	self.public_key = RSA.importKey(public_keydata)
 
+
+
   def verify_signature(self, public_key, ciphertext):
+	#print ("Public Key Object={0}".format(public_key))
     signer = PKCS1_v1_5.new(public_key)
+
     result = signer.verify(self.digest, b64decode(ciphertext))
     return result
 
 
-def launch (keydir='/home/mininet/pox/keys/'):
+def launch (keydir=None):
 
   from pox.lib.recoco import Timer
 
